beb1e51e4d3212003ac0894b3dda6a71167bd6b9
Valdomiro Santos <valdomiro.jose@outlook.com>
2024-09-13 11:03:46 -0300

    ate criacao do menu

---
+++ src/app/components/menu-title/menu-title.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-title',
+  templateUrl: './menu-title.component.html',
+  styleUrl: './menu-title.component.scss'
+})
+export class MenuTitleComponent {
+
+}
