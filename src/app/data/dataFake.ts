b580446b4e09c3191adbb75ed91d4dba78f515a3
Valdomiro Santos <valdomiro.jose@outlook.com>
2024-09-15 05:52:02 -0300

    projeto finalizado

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -10,6 +10,25 @@ export const dataFake = [
     'photoCover': 'https://soubh.uai.com.br/uploads/post/image/11974/main_Divertida-Mente-Pixar.jpg',
     'title': 'Divertida Mente 2',
     'description': 'Depois do sucesso de “Divertida Mente”, lançado em 2015, a sequência é, sem dúvidas, um dos filmes infantis mais esperados para 2024. Na trama, Riley cresceu e agora já completou 13 anos, tendo corpo e mente de pré-adolescente. Então, Alegria, Raiva, Medo, Nojo e Tristeza serão apresentados a novas emoções que vem junto com o período da adolescência. No trailer, já conhecemos a primeira delas: a Ansiedade. Mas também há expectativa de que Inveja, Tédio e Vergonha também acompanhem a protagonista ao longo do filme.'
-  }
+  },
+  {
+    'id': '3',
+    'photoCover': 'https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg',
+    'title': 'Lançamentos da Marvel',
+    'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit iusto cumque minus beatae maiores ea earum quod accusantium laborum dignissimos, deleniti quo harum suscipit, molestiae aut corrupti itaque veniam dicta.Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit iusto cumque minus beatae maiores ea earum quod accusantium laborum dignissimos, deleniti quo harum suscipit, molestiae aut corrupti itaque veniam dicta. DLorem ipsum dolor sit amet consectetur adipisicing elit. Fugit iusto cumque minus beatae maiores ea earum quod accusantium laborum dignissimos, deleniti quo harum suscipit, molestiae aut corrupti itaque veniam dicta.Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit iusto cumque minus beatae maiores ea earum quod accusantium laborum dignissimos, deleniti quo harum suscipit, molestiae aut corrupti itaque veniam dicta.'
+  },
+  {
+    'id': '4',
+    'photoCover': 'https://www.apple.com/tv-pr/shows-and-films/s/silo/images/season-01/show-home-graphic-header/key-art-02/4x1/Apple_TV_Silo_key_art_graphic_header_4_1_show_home.jpg.og.jpg?1718104845661',
+    'title': 'Silo',
+    'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit iusto cumque minus beatae maiores ea earum quod accusantium laborum dignissimos, deleniti quo harum suscipit, molestiae aut corrupti itaque veniam dicta.Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit iusto cumque minus beatae maiores ea earum quod accusantium laborum dignissimos, deleniti quo harum suscipit, molestiae aut corrupti itaque veniam dicta. DLorem ipsum dolor sit amet consectetur adipisicing elit. Fugit iusto cumque minus beatae maiores ea earum quod accusantium laborum dignissimos, deleniti quo harum suscipit, molestiae aut corrupti itaque veniam dicta.Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit iusto cumque minus beatae maiores ea earum quod accusantium laborum dignissimos, deleniti quo harum suscipit, molestiae aut corrupti itaque veniam dicta.'
+  },
+  {
+    'id': '5',
+    'photoCover': 'https://xvcuritiba.com.br/wp-content/uploads/2017/12/dark1.jpg.webp',
+    'title': 'Dark',
+    'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit iusto cumque minus beatae maiores ea earum quod accusantium laborum dignissimos, deleniti quo harum suscipit, molestiae aut corrupti itaque veniam dicta.Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit iusto cumque minus beatae maiores ea earum quod accusantium laborum dignissimos, deleniti quo harum suscipit, molestiae aut corrupti itaque veniam dicta. DLorem ipsum dolor sit amet consectetur adipisicing elit. Fugit iusto cumque minus beatae maiores ea earum quod accusantium laborum dignissimos, deleniti quo harum suscipit, molestiae aut corrupti itaque veniam dicta.Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit iusto cumque minus beatae maiores ea earum quod accusantium laborum dignissimos, deleniti quo harum suscipit, molestiae aut corrupti itaque veniam dicta.'
+  },
+  
 
 ]
